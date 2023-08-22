737d2bcd01e009178296d4253a21b92070f959a1
lorenzo-neri <lorenzo21neri@gmail.com>
2023-08-22 14:18:06 +0200

    add prompt in js and #password_generator in html

---
+++ assets/js/main.js
@@ -0,0 +1,24 @@
+//Chiedi all’utente il suo nome, poi chiedi il suo cognome, poi chiedi il suo colore preferito ed infine scrivi in pagina (non solo in console) il risultato, che va formattato in questo modo: nomecognomecolorepreferito23
+
+//strumenti
+
+/* 
+- prompt
+- document.getElementById
+- console.log
+*/
+
+//chiedere il nome utente
+const userFirstName = prompt('Ciao, per favore, inserisci il tuo nome');
+
+//chiedere il cognome utente
+const userLastName = prompt ('Adesso, inserisci il tuo cognome')
+
+//chiedere il colore preferito
+const userFavouriteColor = prompt ('Ci siamo quasi, inserisci il tuo colore preferito')
+
+//selezionare i tag tramite id in cui inserire i risultati
+
+//stampare il risultato in console
+
+//stampare il risultato in pagina
